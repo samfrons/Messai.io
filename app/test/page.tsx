620c3645fa54c33ee79d62dc9edf0017bd831359
Sam Frons <samfrons@gmail.com>
2025-07-11 05:33:42 +0200

    removed files

--- app/test/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function TestPage() {
-  return (
-    <div className="min-h-screen bg-blue-500 text-white flex items-center justify-center">
-      <h1 className="text-4xl">Test Page - Server is Working!</h1>
-    </div>
-  )
-}
